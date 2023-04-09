59cef7a4f2ef3d22f3f1ed24904cad113930529a
ilhamarifinsamsul <ailhamsamsul@gmail.com>
2023-04-09 03:23:30 -0700

    menambahkan lorem

--- app/Views/welcome_sila.php
+++ app/Views/welcome_sila.php
@@ -10,6 +10,8 @@
 
 <body>
     <h1>Welcome Project SILA</h1>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum corrupti earum cum dolores et officiis dolore tempore, nisi explicabo eius dignissimos quidem sint ipsa consequuntur repudiandae autem molestias, sit similique.</p>
 </body>
 
 </html>
