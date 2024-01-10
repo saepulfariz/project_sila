81f1436757324c46eec7f18bdddb531e2f2ad959
saepulfariz <saepulfariz108@gmail.com>
2024-01-11 06:48:48 +0700

    fix not open navbar frontend

--- app/Views/template/front/index.php
+++ app/Views/template/front/index.php
@@ -35,7 +35,7 @@ $alert = new App\Libraries\Alert();
 
 
 
-    <script src="<?= base_url(); ?>assets/front/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe">
+    <script src="<?= base_url(); ?>assets/front/js/bootstrap.bundle.min.js">
     </script>
 
     <script src="<?= base_url(); ?>assets/front/js/jquery-3.6.4.js"></script>
